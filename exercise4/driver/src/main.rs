975bab0a5e53a9edcbfc101b2609cf0067d06e3a
Corey Farwell <coreyf@rwell.org>
2016-10-26 14:12:21 -0400

    Older rust-openssl for now

--- exercise4/driver/src/main.rs
+++ exercise4/driver/src/main.rs
@@ -5,7 +5,7 @@ extern crate openssl;
 fn main() {
     afl::handle_bytes(|bytes| {
         let ring_digest = ring::digest::digest(&ring::digest::SHA256, &bytes);
-        let openssl_digest = openssl::hash::hash(openssl::hash::MessageDigest::sha256(), &bytes).unwrap();
+        let openssl_digest = openssl::crypto::hash::hash(openssl::crypto::hash::Type::SHA256, &bytes);
         assert_eq!(ring_digest.as_ref(), &openssl_digest[..]);
     });
 }
